c0dec4976d33e3238845e52092b3719fdca947fb
HML2016 <hml19941204@126.com>
2018-03-16 22:12:31 +0800

    添加自动搜索可用串口

--- SerialCommunicate/Form1.Designer.cs
+++ SerialCommunicate/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
@@ -55,7 +55,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.panel2);
             this.groupBox1.Controls.Add(this.panel1);
             this.groupBox1.Controls.Add(this.label3);
@@ -72,16 +72,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "设置";
             // 
-            // button2
+            // button4
             // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(105, 176);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(74, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "关闭端口";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button4.Location = new System.Drawing.Point(129, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 20);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "扫描";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -165,9 +163,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 176);
+            this.button1.Location = new System.Drawing.Point(54, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.Size = new System.Drawing.Size(77, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "打开端口";
             this.button1.UseVisualStyleBackColor = true;
@@ -195,10 +193,11 @@
             "14400",
             "19200",
             "115200"});
-            this.comboBox2.Location = new System.Drawing.Point(66, 60);
+            this.comboBox2.Location = new System.Drawing.Point(54, 60);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(111, 20);
             this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -213,9 +212,9 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(66, 20);
+            this.comboBox1.Location = new System.Drawing.Point(54, 20);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(111, 20);
+            this.comboBox1.Size = new System.Drawing.Size(60, 20);
             this.comboBox1.TabIndex = 1;
             // 
             // 端口
@@ -236,7 +235,6 @@
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(290, 205);
             this.textBox1.TabIndex = 2;
-            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button3
             // 
@@ -302,9 +300,9 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- SerialCommunicate/Form1.cs
+++ SerialCommunicate/Form1.cs
@@ -20,32 +20,63 @@ namespace SerialCommunicate
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            if (serialPort1.IsOpen)
             {
-                serialPort1.PortName = comboBox1.Text;
-                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text,10);//十进制数据转换
-                serialPort1.Open();
-                button1.Enabled = false;//打开串口按钮不可用
-                button2.Enabled = true;//关闭串口
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch { }
+                ovalShape1.FillColor = Color.Gray;
+                button1.Text = "打开端口";
             }
-            catch {
-                MessageBox.Show("端口错误,请检查串口", "错误");
+            else
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text, 10);//十进制数据转换
+                    serialPort1.Open();
+                    ovalShape1.FillColor = Color.Green;
+                    button1.Text = "关闭端口";
+                }
+                catch
+                {
+                    MessageBox.Show("端口错误,请检查串口", "错误");
+                }
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 1; i < 20; i++)
-            {
-                comboBox1.Items.Add("COM" + i.ToString());
-            }
-            comboBox1.Text = "COM1";//串口号多额默认值
+            ovalShape1.FillColor = Color.Gray;
+            SearchAndAddSerialToComboBox(serialPort1, comboBox1);//扫描可用端口
             comboBox2.Text = "4800";//波特率默认值
             
             /*****************非常重要************************/
             
             serialPort1.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);//必须手动添加事件处理程序
 
+        }
+        private void SearchAndAddSerialToComboBox(SerialPort MyPort, ComboBox MyBox)//扫描可用端口
+        {                                                               //将可用端口号添加到ComboBox
+            string Buffer;                                              //缓存
+            MyBox.Items.Clear();                                        //清空ComboBox内容
+            for (int i = 1; i < 20; i++)                                //循环
+            {
+                try                                                     //核心原理是依靠try和catch完成遍历
+                {
+                    Buffer = "COM" + i.ToString();
+                    MyPort.PortName = Buffer;
+                    MyPort.Open();                                      //如果失败，后面的代码不会执行
+                                                                        // MyString[count] = Buffer;
+                    MyBox.Items.Add(Buffer);                            //打开成功，添加至下俩列表
+                    MyPort.Close();                                     //关闭
+                }
+                catch
+                {
+                }
+            }
         }
 
         private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)//串口数据接收事件
@@ -73,20 +104,6 @@ namespace SerialCommunicate
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                serialPort1.Close();//关闭串口
-                button1.Enabled = true;//打开串口按钮可用
-                button2.Enabled = false;//关闭串口按钮不可用
-            }
-            catch (Exception err)//一般情况下关闭串口不会出错，所以不需要加处理程序
-            {
-
-            }
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             byte[] Data = new byte[1];//作用同上集
@@ -104,20 +121,19 @@ namespace SerialCommunicate
                         {
                             MessageBox.Show("串口数据写入错误", "错误");//出错提示
                             serialPort1.Close();
-                            button1.Enabled = true;//打开串口按钮可用
-                            button2.Enabled = false;//关闭串口按钮不可用
+                            button1.Text = "打开端口";
                         }
                     }
                     else
                     {
-                        for (int i = 0; i < (textBox2.Text.Length - textBox2.Text.Length % 2) / 2; i++)//取余3运算作用是防止用户输入的字符为奇数个
+                        for (int i = 0; i < (textBox2.Text.Length - textBox2.Text.Length % 2) / 2; i++)//取余运算作用是防止用户输入的字符为奇数个
                         {
                             Data[0] = Convert.ToByte(textBox2.Text.Substring(i * 2, 2), 16);
                             serialPort1.Write(Data, 0, 1);//循环发送（如果输入字符为0A0BB,则只发送0A,0B）
                         }
                         if (textBox2.Text.Length % 2 != 0)//剩下一位单独处理
                         {
-                            Data[0] = Convert.ToByte(textBox2.Text.Substring(textBox2.Text.Length-1, 1), 16);//单独发送B（0B）
+                            Data[0] = Convert.ToByte(textBox2.Text.Substring(textBox2.Text.Length - 1, 1), 16);//单独发送B（0B）
                             serialPort1.Write(Data, 0, 1);//发送
                         }
                    }
@@ -125,5 +141,9 @@ namespace SerialCommunicate
             }
         }
 
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
